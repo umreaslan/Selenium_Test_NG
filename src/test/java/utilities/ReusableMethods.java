d50232a394cd783b6272c389a3c14f652bb1c85c
unknown <sevdeneda.38@gmail.com>
2023-03-01 15:52:44 +0300

    01.03.23

--- src/test/java/utilities/ReusableMethods.java
+++ src/test/java/utilities/ReusableMethods.java
@@ -1,6 +1,12 @@
 package utilities;
 
+import org.openqa.selenium.WebElement;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ReusableMethods {
+
     public static void bekle(int saniye){
 
         try {
@@ -8,6 +14,18 @@ public class ReusableMethods {
         } catch (InterruptedException e) {
 
         }
+    }
+
+    public static List<String> stringListeCevir(List<WebElement> webElementList){
+
+        List<String> stringListesi= new ArrayList<>();
+
+        for (WebElement eachElement: webElementList
+        ) {
+            stringListesi.add(eachElement.getText());
+        }
+
+        return stringListesi;
     }
 
 }
